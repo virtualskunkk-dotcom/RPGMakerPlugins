845dd964742c6c9c56c4f3284c14f05c7de1680f
virtualskunkk-dotcom <virtualskunkk@gmail.com>
2026-02-11 05:03:37 +0200

    Refactor RemoveGuard.js
    Added a comment about modifying the Attack command feature.

--- RemoveGuard.js
+++ RemoveGuard.js
@@ -1,6 +1,6 @@
 /*:
  * @plugindesc Removes Guard command and hides Attack command until TP is full. v1.1
- * @author PixelSkunk
+ * @author VirtualSkunk
  * @help 
  * This plugin removes the Guard command from the battle menu by overriding its addition.
  * It also hides the Attack command until the actor's TP is full (100 TP).
@@ -9,14 +9,15 @@
 
     // Completely remove Guard
     Window_ActorCommand.prototype.addGuardCommand = function() {
-        // Nothing. No Guard.
+        // Nothing.
     };
 
-    // Modify Attack to require full TP
+    // Modify Attack to require full TP (this was a featture specific to my game, you can modify this to your liking!
     Window_ActorCommand.prototype.addAttackCommand = function() {
         if (this._actor && this._actor.tp >= this._actor.maxTp()) {
             this.addCommand(TextManager.attack, 'attack', this._actor.canAttack());
         }
     };
+
 
 })();
