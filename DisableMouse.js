3702d47d70e1ecd02b0a19a20d3774e931289cf8
virtualskunkk-dotcom <virtualskunkk@gmail.com>
2026-02-11 05:00:47 +0200

    Add usage comments to DisableMouse.js
    Added comments to clarify usage of the DisableMouse plugin.

--- DisableMouse.js
+++ DisableMouse.js
@@ -1,1 +1,3 @@
+//This is a simple code snippet to disable the mouse in your game!
+//Simply import this code as a plugin, or add it to an existing one (not recommended unless you know at least a little JavaScript due to plugin compatibility issues)
 TouchInput.update = function() {};
